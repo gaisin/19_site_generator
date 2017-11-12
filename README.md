c629ce3a55bc0c121c832e3ef091fc918deb0eaa
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2017-11-12 14:42:07 +0600

    code functionized, render refreshed

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 # Some useful links
 
-[Working with templates in Jinja2 \[Russian language\]](https://natenka.gitbooks.io/pyneng/content/v/python2.7/book/13_jinja2/3c_syntax_if.html)
-[How to stick footer to the bottom for Bootstrap \[Russian language\]](https://toster.ru/q/19238#answer_311193)
+[Working with templates in Jinja2 \[Russian language\]](https://natenka.gitbooks.io/pyneng/content/v/python2.7/book/13_jinja2/3c_syntax_if.html)  
+[How to stick footer to the bottom for Bootstrap \[Russian language\]](https://toster.ru/q/19238#answer_311193) 
+[Github Pages](https://pages.github.com/)  
+[How to deploy many websites to github.io? \[Russian language\]](https://toster.ru/q/368411)  
 
 # Project Goals
 
--- site_generator.py
+++ site_generator.py
@@ -3,47 +3,84 @@ import markdown
 import os
 import json
 
-# At first parsing config.json
-with open('config.json', 'r', encoding='utf-8') as config_file:
-    config_json = config_file.read()
-    config_dict = json.loads(config_json)
 
-# Creating articles HTML pages
-env = Environment(loader=FileSystemLoader(searchpath='templates'))
+def load_config(config_file):
+    with open('config.json', 'r', encoding='utf-8') as config_file:
+        config_json = config_file.read()
+        config_dict = json.loads(config_json)
+    return config_dict
 
-article_template = env.get_template('article_template.html')
 
-for article in config_dict['articles']:
-    with open('articles/{}'.format(article['source']), 
-                'r', 
-                encoding='utf-8') as md_file:
-        markdown_content = md_file.read()
+def load_markdown(source):
+    with open('articles/{}'.format(source),
+                    'r',
+                    encoding='utf-8') as md_file:
+            markdown_content = md_file.read()
+    return markdown_content
 
-    article_content = markdown.markdown(markdown_content)
-    article_title = article['title']
-    article_basename = os.path.basename(article['source'])
-    article_name_without_extension = os.path.splitext(article_basename)[0]
-    article['html_link'] = '{}.html'.format(article_name_without_extension)
 
-    with open('docs/articles/{}.html'.format(article_name_without_extension), 
-                'w', 
-                encoding='utf-8') as html_page:
-        html_page.write(article_template.render(
-                        md_content=article_content,
-                        md_title=article_title,
-                        static_path = '../static',
-                        title = article_title))
+def convert_markdown_to_html(markdown_content):
+    html_content = markdown.markdown(markdown_content)
+    return html_content
 
-# Creating index HTML page
-index_template = env.get_template('index_template.html')
 
-with open('docs/index.html', 'w', encoding='utf-8') as index:
-    index.write(index_template.render(
-                                topics = config_dict['topics'], 
-                                articles = config_dict['articles'], 
-                                static_path = 'static',
-                                title = 'Энциклопедия начинающего питониста'))
+def save_to_html(filepath, content):
+    with open(filepath, 'w', encoding='utf-8') as html_page:
+        html_page.write(content)
 
-# Доделать:
-# выложить на github pages
-# попробовать сделать livereload
+
+def render_article_html(template, content, title, static_path):
+    return template.render(
+                    md_content=content,
+                    md_title=title,
+                    static_path=static_path)
+
+
+def render_index_html(template, topics, articles, static_path, title):
+    return template.render(
+                    topics=topics,
+                    articles=articles,
+                    static_path=static_path,
+                    title=title)
+
+
+if __name__ == '__main__':
+    config_dict = load_config('config.json')
+
+    env = Environment(loader=FileSystemLoader(searchpath='templates'))
+
+    article_template = env.get_template('article_template.html')
+
+    for article in config_dict['articles']:
+        markdown_content = load_markdown(article['source'])
+        article_html_content = convert_markdown_to_html(markdown_content)
+
+        article_title = article['title']
+        article_basename = os.path.basename(article['source'])
+        article_name_without_extension = os.path.splitext(article_basename)[0]
+        article['html_link'] = '{}.html'.format(article_name_without_extension)
+
+        article_savepath = 'docs/articles/{}.html'\
+                                .format(article_name_without_extension)
+
+        article_content_to_save = render_article_html(
+                                article_template,
+                                article_html_content,
+                                article_title,
+                                '../static')
+
+        save_to_html(article_savepath, article_content_to_save)
+
+    # Creating index HTML page
+    index_template = env.get_template('index_template.html')
+
+    index_content_to_save = render_index_html(
+                                index_template,
+                                config_dict['topics'],
+                                config_dict['articles'],
+                                'static',
+                                'Энциклопедия начинающего питониста')
+
+    index_savepath = 'docs/index.html'
+
+    save_to_html(index_savepath, index_content_to_save)
